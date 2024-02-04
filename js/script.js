07426d0d096c9ca52cf25519f5acc3d87d1ffbfc
AdiSan103 <arwinsmith01@gmail.com>
2024-02-04 13:14:44 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 // Tentukan endpoint API untuk data resep makanan
-const apiUrl = 'http://128.199.167.159/v1/idc/food-receipts';
+const apiUrl = "http://128.199.167.159/v1/idc/food-receipts";
 
 // Buat permintaan GET menggunakan Fetch API
 fetch(apiUrl)
-  .then(response => {
+  .then((response) => {
     if (!response.ok) {
-      throw new Error('Respon jaringan tidak ok');
+      throw new Error("Respon jaringan tidak ok");
     }
     return response.json();
   })
-  .then(foodData => {
+  .then((foodData) => {
     // Proses data resep makanan yang diambil
-    console.log('Data Resep Makanan:', foodData);
+    console.log("Data Resep Makanan:", foodData);
   })
-  .catch(error => {
-    console.error('Error:', error);
+  .catch((error) => {
+    console.error("Error:", error);
   });
